1a7a7a10f33f94115f3aa570fabd9ab540894d3f
monirulislamkhan <mr.monirulkhan@gmail.com>
2025-01-16 00:07:16 +0530

    add type conversion examples in JavaScript

---
+++ js/variables-data-type/type-conversion.js
@@ -0,0 +1,12 @@
+let amount = 100;
+
+/* Convert string to number */
+amount = parseInt(amount);
+amount = +amount;
+amount = +amount;
+amount = Number(amount);
+
+/* Convert number to string */
+amount = amount.toString();
+
+console.log(amount, typeof amount);
