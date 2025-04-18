8e3ff3b105f341ec163240a0a6b61eb6085a4e80
monirulislamkhan <mr.monirulkhan@gmail.com>
2025-04-19 00:15:23 +0530

    Form validation normal work done

--- form-validation/form-v1/script.js
+++ form-validation/form-v1/script.js
@@ -4,7 +4,12 @@ const errorMgs = document.getElementById('errorMessage');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  const name = document.getElementById('name');
-  const email = document.getElementById('email');
-  console.log(name);
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  if (name === '' || email === '') {
+    alert('Please fill the name and email');
+  } else {
+    alert('Thank you');
+    console.log(name, email);
+  }
 });
