13bed56772e461bb9f4df6f3a361428685e87571
monirulislamkhan <mr.monirulkhan@gmail.com>
2025-04-19 00:02:59 +0530

    Some changes

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -23,7 +23,59 @@ const restaurant = {
   },
 };
 
-// Array Destructuring
+// Spread Operator
+// What is spread operator?
+// answer: It is used to spread the elements of an array or an object into another array or an object.
+
+/* const arr = [7, 8, 9];
+const newArr = [1, 2, 3, 4, 5, 6, ...arr];
+console.log(newArr); */
+
+/* const menu = [...restaurant.categories, 'Indian'];
+console.log(menu); */
+
+/* const mainMenuCopy = [...restaurant.mainMenu, 'Extra'];
+console.log(mainMenuCopy);
+console.log(restaurant.mainMenu);
+ */
+
+// Real world example
+
+// Destructuring Objects
+// What is Object Destructuring?
+// Answer: It is used to destructure an object into individual variables.
+
+const { name, location, categories, starterMenu, mainMenu } = restaurant;
+// console.log(name, location, categories, starterMenu, mainMenu);
+
+// Object Destructuring with rename
+const { name: restaurantName, location: localities, categories: foodType } = restaurant;
+const { openingHours } = restaurant;
+
+// Default values
+/* const { menu = {} } = restaurant;
+console.log(menu); */
+
+// Mutating variables
+/* let a = 111;
+let b = 999;
+
+const obj = { a: 112, b: 998 };
+console.log(a, b);
+({ a, b } = obj);
+console.log(a, b); */
+
+// Nested Objects
+
+/* // const { thu, fri, sat } = openingHours;
+console.log(thu); */
+
+/* const {
+  sat: { open, close },
+} = openingHours;
+console.log(open, close); */
+
+/* // Array Destructuring
 let [main, , second] = restaurant.categories;
 console.log(main, second);
 
@@ -65,4 +117,13 @@ const isEmpty = function (obj) {
   return Object.keys(obj).length > 0;
 };
 
-console.log(isEmpty());
+console.log(isEmpty([]));
+ */
+// Nested
+/* const [d, e, f, [g, h]] = [1, 2, 3, [4, 5]];
+console.log(d, e, f, g, h); */
+
+// Default Parameter
+
+/* const [a, b, c, d, e = 10, f = 0] = restaurant.starterMenu;
+console.log(a, b, c, d, e, f); */
